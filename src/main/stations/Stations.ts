c7d87ee1bdaba0c641f651ad10add2206ca440aa
Klaus Reimer <k@ailis.de>
2020-11-28 10:16:16 +0100

    Add stations by system index

--- src/main/stations/Stations.ts
+++ src/main/stations/Stations.ts
@@ -6,6 +6,7 @@
 import { Serializable } from "@kayahr/kaylib/lib/main/lang/Serializable";
 import * as readline from "readline";
 
+import { System } from "../systems/System";
 import { Factories } from "../util/Factories";
 import { NamedEntities } from "../util/NamedEntities";
 import { Station, StationJSON } from "./Station";
@@ -13,6 +14,20 @@ import { Station, StationJSON } from "./Station";
 export type StationsJSON = StationJSON[];
 
 export class Stations extends NamedEntities<Station> implements Serializable<StationsJSON> {
+    private readonly systemStations = new Map<number, Station[]>();
+
+    protected add(station: Station): void {
+        super.add(station);
+
+        const systemId = station.getSystemId();
+        let systemStations = this.systemStations.get(systemId);
+        if (systemStations == null) {
+            systemStations = [];
+            this.systemStations.set(systemId, systemStations);
+        }
+        systemStations.push(station);
+    }
+
     public static fromJSON(json: StationsJSON, factories?: Factories): Stations {
         const stations = new Stations();
         json.forEach(json => {
@@ -32,5 +47,19 @@ export class Stations extends NamedEntities<Station> implements Serializable<Sta
 
     public toJSON(): StationsJSON {
         return this.entities.map(entity => entity.toJSON());
+    }
+
+    /**
+     * Returns all stations for the given system.
+     *
+     * @param system - The system or system ID.
+     * @return All stations for the given system.
+     */
+    public getBySystem(system: System | number): Station[] {
+        const stations = this.systemStations.get(system instanceof System ? system.getId() : system);
+        if (stations == null) {
+            return [];
+        }
+        return stations.slice();
     }
 }
